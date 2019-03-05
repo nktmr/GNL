a5013dd8a83d5e6bfc35e3015d02f7438b4e9877
Daf <daphenie.felissaint@gmail.com>
2019-03-05 09:40:54 +0100

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -1,24 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfelissa <dfelissa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/19 21:54:38 by dfelissa          #+#    #+#             */
+/*   Updated: 2019/02/22 22:16:23 by dfelissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../Libft/libft.h"
 #include "get_next_line.h"
-#define BUF_SIZE 20
+#define BUFF_SIZE 100000
 
 int			get_next_line(const int fd, char **line)
 {
 	int			ret;
-	static char		*tmp;
-	char			buf[BUF_SIZE + 1];
-	char			*str;
-
-	if (!tmp)
-		tmp = ft_strnew(1);
-	while (!ft_strchr(tmp, '\n') && (ret = read(fd, buf, BUF_SIZE)))
+	static char		*str[_SC_OPEN_MAX];
+	char			buf[BUFF_SIZE + 1];
+	char			*tmp;
+	
+	if (fd < 0 || line == NULL || BUFF_SIZE == 0 || fd > _SC_OPEN_MAX
+			|| read(fd, "", 0))
+		return (-1);
+	if (!str[fd])
+		str[fd] = ft_strnew(1);
+	while (!ft_strchr(str[fd], '\n') && (ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		tmp = ft_strjoin(tmp, buf);
+		tmp = ft_strjoin(str[fd], buf);
+		ft_strdel(&str[fd]);
+		str[fd] = tmp;
 	}
-	*line = ft_strsub(tmp, 0, ft_strnlen(tmp, '\n'));
-	tmp = ft_strsub(tmp, ft_strnlen(tmp,'\n') + 1 , ft_strlen(&tmp[ft_strnlen(tmp, '\n')]));
-	str = ft_strjoin(*line, tmp);
-	ft_putstr(str);
-	return (0);
+	if (ft_strlen(str[fd]) == 0)
+		return (0);
+	*line = ft_strsub(str[fd], 0, ft_strnlen(str[fd], '\n'));
+	tmp = ft_strsub(str[fd], ft_strnlen(str[fd],'\n') + 1 ,
+			ft_strlen(&str[fd][ft_strnlen(str[fd], '\n')]));
+	ft_strdel(&str[fd]);
+	str[fd] = tmp;
+	return (1);
 }
--- main.c
+++ main.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfelissa <dfelissa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/19 21:55:34 by dfelissa          #+#    #+#             */
+/*   Updated: 2019/02/22 18:56:28 by dfelissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../Libft/libft.h"
 #include "get_next_line.h"
 
 int main(int ac, char **av)
 {
 	int	fd;
-	char		**line;
+	char	*line;
 
-	if(!(line = (char**)malloc(sizeof(char*) * 2)))
-		return (0);
+	line = NULL;
 	fd = open(av[1], O_RDONLY);
-	get_next_line(fd, line);
+	while (get_next_line(fd, &line) == 1)
+		ft_putendl(line);
 	return(0);
 }
